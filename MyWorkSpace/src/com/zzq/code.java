25ca661eca1f40400b95c8403c5cac6e6704f50f
zhangzhiqiang <zhangzhiqiang@newstrength.cn>
2023-09-04 13:08:35 +0800

    Initial commit

---
+++ MyWorkSpace/src/com/zzq/code.java
@@ -0,0 +1,11 @@
+package com.zzq;
+
+import java.io.File;
+
+public class code {
+    public static void main(String[] args) {
+        File file = new File("F:\\TestDir");
+        file.mkdir();
+        System.err.println("11234");
+    }
+}
