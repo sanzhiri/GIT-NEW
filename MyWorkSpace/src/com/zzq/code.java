10f72f3aeb7c05b1595041abe5ca2fdc2f10cece
sanzhiri <109023238+sanzhiri@users.noreply.github.com>
2023-12-04 10:14:08 +0800

    Update code.java

--- MyWorkSpace/src/com/zzq/code.java
+++ MyWorkSpace/src/com/zzq/code.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class code {
     public static void main(String[] args) {
-        File file = new File("F:\\TestDir");
+        File file = new File("/usr/local/TestDir");
         file.mkdir();
         System.err.println("11234");
     }
